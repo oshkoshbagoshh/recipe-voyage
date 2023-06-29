3d9ed397ad7bafeb4ad8a45f3cc65069f8162a25
karthikmurugappan <karthikmurugappan7@gmail.com>
2023-06-29 12:28:02 -0400

    file structure update

--- server.js
+++ server.js
@@ -42,5 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () =>
+    console.log(`Now listening http://localhost:${PORT}/`)
+  );
 });
